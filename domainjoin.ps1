4cf1cfdb38088752d3aadc8a1f421c1304323464
pardhasaradhireddy <39433011+pardhasaradhireddy@users.noreply.github.com>
2021-05-10 03:20:48 +0000

    Add files via upload

---
+++ domainjoin.ps1
@@ -0,0 +1,25 @@
+﻿# Define clear text string for username and password
+#[string]$userName = 'dmiuser'
+#[string]$userPassword = 'dmiuser@2021'
+#[string]$domainname = 'corp.testdomain.com'
+
+
+[CmdletBinding()]
+param(
+  [Parameter(Mandatory=$true)]
+  [String]$domainuserName,
+
+  [String]$domainPassword,
+
+  [Parameter(Mandatory=$true)]
+  [String]$domainname
+
+)
+
+
+# Convert to SecureString
+[securestring]$secStringPassword = ConvertTo-SecureString $domainPassword -AsPlainText -Force
+
+[pscredential]$credObject = New-Object System.Management.Automation.PSCredential ($domainuserName, $secStringPassword)
+
+Add-Computer -DomainName $domainname -Credential $credObject
